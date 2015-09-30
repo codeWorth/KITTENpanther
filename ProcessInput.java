46c7f4e8bea1fc834e8385ab823238ffeadd2b66
Legionof7 <justin.zheng@gmail.com>
2015-09-30 16:40:58 -0700

    Update ProcessInput.java

--- ProcessInput.java
+++ ProcessInput.java
@@ -3,3 +3,51 @@ public class ProcessInput {
 		String inLower = in.toLowerCase();
 	}
 }
+
+public class ProcessInput {
+	public static void interpretInput(String in){
+		String inLower = in.toLowerCase();
+	}
+
+	public static void printTasks(Task[] tasks){
+		int i = 1;
+		for (Task task : tasks){
+			String lePrint = "";
+
+			lePrint = lePrint + i + ". ";
+
+			lePrint = lePrint + task.name + "______________";
+
+			lePrint = lePrint + task.daysUntilDue;
+
+			i++;
+			System.out.println (lePrint);
+
+		}
+	} 
+
+	public static void printTasksWithDue(Task[] tasks){
+		int i = 1;
+		for (Task task : tasks){
+			String lePrint = "";
+
+			lePrint = lePrint + i + ". ";
+
+			if (daysUntilDue = 0)
+				system.out.println ("DUE!!!");
+			if (daysUntilDue < 0)
+				system.out.println ("IS LATE!!! EEK! MEOW! EEK! IT'S LATE!!! AGGGGGGHHHHBAHBAH!!!  ITS LATE!!!")
+
+
+			lePrint = lePrint + task.name + "______________";
+
+			lePrint = lePrint + task.daysUntilDue;
+
+			i++;
+
+			System.out.println (lePrint);
+			System.out.println (daysUntilDue);
+	}
+}
+  
+                             
