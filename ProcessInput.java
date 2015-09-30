e197a097a963d1054c5700efcbd1e6dff0a2c608
Drewpew <agcummings11@gmail.com>
2015-09-30 16:54:59 -0700

    Update ProcessInput.java

--- ProcessInput.java
+++ ProcessInput.java
@@ -1,10 +1,44 @@
 public class ProcessInput {
-	public static void interpretInput(String in){
-		String inLower = in.toLowerCase();
-	}
-}
+	public static void interpretInput(Tasks tasks){
+		Scanner scanner = new Scanner(System.in);
 
-public class ProcessInput {
+		String in = scanner.nextLine();
+
+		String inLower = in.toLowerCase();
+
+		String[] words = inLower.split(" ");
+
+		if (words.length == 0){
+			return;
+		}
+
+		String firstWord = words[0];
+
+		if (firstWord.equals("add") || firstWord.equals("addtask")){
+			System.out.println("Enter the name: ");
+			String theName = scanner.nextLine();
+
+			System.out.println("Enter a description: ");
+			String desc = scanner.nextLine();
+
+			System.out.println("Enter the number of days until this is due: ");
+			int untilDue = scanner.nextInt();
+
+			System.out.println("Enter the priority: ");
+			int prio = scanner.nextInt();
+
+			Task newTask = new Task(theName, desc, prio, untilDue);
+
+			tasks.addtask(newTask);
+		} else if (firstWord.equals("today")){
+			ProcessInput.printTasks(tasks.tasksForToday());
+		} else if (firstWord.equals("all") || firstWord.equals("alltasks")){
+			ProcessInput.printTasksWithDue(tasks.allTasks());
+		}  else if (firstWord.equals("finish")){
+			System.out.println("");
+		}
+	}
+
 	public static void interpretInput(String in){
 		String inLower = in.toLowerCase();
 	}
