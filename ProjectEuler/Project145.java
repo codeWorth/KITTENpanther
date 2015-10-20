d0860ec8f6418e99fdfa842bef5974850181cb0c
Drewpew <agcummings11@gmail.com>
2015-10-20 08:51:23 -0700

    Update Project145.java

--- ProjectEuler/Project145.java
+++ ProjectEuler/Project145.java
@@ -1,21 +1,20 @@
 public class Project145{
   public static ArrayList<int[]> situations(int numOfDigits, ArrayList<int[]> prevSits){
     /*
-    0 = even < 10
-    1 = even >= 10
-    2 = odd < 10
-    3 = odd >= 10
-    */
-    if (numOfDigits == 0){
-      return prevSits;
-    } else 
-      for (int i = 0; i < prevSits.size(); i++){
-        prevSits.add(0);
-        prevSits.add(1);
-        prevSits.add(2);
-        prevSits.add(3);
-      }
-      return situations(numOfDigits-1, prevSits);
-    }
-  }
+		0 = even < 10
+		1 = even >= 10
+		2 = odd < 10
+		3 = odd >= 10
+		*/
+		if (numOfDigits == 0){
+			return prevSits;
+		} else {
+			ArrayList<int[]> newSits = new ArrayList<int[]>();
+			for (int i = 0; i < prevSits.size(); i++){
+				int[] prevState = prevSits.get(i);
+				prevSits.add
+				prevSits.get(i)[numOfDigits-1] = 0;
+			}
+			return situations(numOfDigits-1, prevSits);
+		}
 }
